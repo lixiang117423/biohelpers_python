bd523f38f799b498380a81eec26c66ddbb5f1b1e
lixiang117423 <lixiang117423@gmail.com>
2025-04-01 08:57:17 +0800

    Remove .PRJNA510920.meta.txt and run_hisat2.sh

--- run_hisat2.sh
+++
@@ -1,4 +0,0 @@
-hisat2 -x example/acuce.genome.hisat2.index -1 example/R24129662_1.fq.gz -2 example/R24129662_2.fq.gz -p 60 | samtools sort -@ 60 -O BAM -o mapping/R24129662.sorted.bam -
-hisat2 -x example/acuce.genome.hisat2.index -1 example/R24129663_1.fq.gz -2 example/R24129663_2.fq.gz -p 60 | samtools sort -@ 60 -O BAM -o mapping/R24129663.sorted.bam -
-hisat2 -x example/acuce.genome.hisat2.index -1 example/R24129662_1.fq.gz -2 example/R24129662_2.fq.gz -p 60 | samtools sort -@ 60 -O BAM -o mapping/R24129662.sorted.bam -
-hisat2 -x example/acuce.genome.hisat2.index -1 example/R24129663_1.fq.gz -2 example/R24129663_2.fq.gz -p 60 | samtools sort -@ 60 -O BAM -o mapping/R24129663.sorted.bam -
